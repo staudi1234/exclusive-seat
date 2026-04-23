c6af909d33d061f44f08cab5cec89b8615be7234
staudi1234 <staudachernoah7@gmail.com>
2026-04-23 15:34:27 +0200

    Delete app/layout.tsx

--- app/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div style={{ padding: 20, fontFamily: "sans-serif" }}>
-      <h1>Exclusive Seat</h1>
-      {children}
-    </div>
-  );
-}
